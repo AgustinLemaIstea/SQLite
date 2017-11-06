b65f1ebdd0a721b1ade96a6ae8e04fee38b17c07
unknown <agustin.lema@istea.com.ar>
2017-11-06 19:13:00 -0300

    Agregada notificacion

--- app/src/main/java/com/example/educacionit/sqlite/ListActivity.java
+++ app/src/main/java/com/example/educacionit/sqlite/ListActivity.java
@@ -1,8 +1,12 @@
 package com.example.educacionit.sqlite;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -19,6 +23,7 @@ public class ListActivity extends AppCompatActivity {
 
     Button btnAdd;
     ListView lvUsuarios;
+    private List<Usuario> usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class ListActivity extends AppCompatActivity {
         setupUI();
         setupButton();
         initializeListView();
+        showNotification();
     }
 
     @Override
@@ -54,7 +60,7 @@ public class ListActivity extends AppCompatActivity {
     private void initializeListView() {
         DBHelper dbHelper = new DBHelper(ListActivity.this);
 
-        final List<Usuario> usuarios = dbHelper.obtenerUsuarios();
+        usuarios = dbHelper.obtenerUsuarios();
 
         UsuariosAdapter adapter = new UsuariosAdapter(ListActivity.this, R.layout.item_usuario, usuarios);
         lvUsuarios.setAdapter(adapter);
@@ -66,5 +72,23 @@ public class ListActivity extends AppCompatActivity {
                 Toast.makeText(ListActivity.this, usuarios.get(position).getNombre(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void showNotification(){
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(ListActivity.this);
+        builder.setContentTitle("Bienvenido de vuelta")
+                .setContentText(String.format("Tenés %d usuarios registrados. Agrega uno más!",usuarios.size()))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+
+        Intent intent = new Intent(ListActivity.this, ElementEditActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(ListActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);//pending intent
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager managerCompat = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);//obtengo el servicio de notificaciones y se lo pongo al manager
+        managerCompat.notify(1, builder.build());    //le paso un id y el builder
+
     }
 }
